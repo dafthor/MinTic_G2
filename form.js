8c9009b33bb753d096fb3903fc6cf6f95b566cda
dafthor <dafthor@gmail.com>
2021-09-23 11:42:50 -0500

    creacion de form.js

---
+++ form.js
@@ -0,0 +1,17 @@
+//DOCUMENTO CON FUNCIONES DE VALIDACION DE FORMULARIO PARA SPRINT 2
+
+function checkUsername (valor){
+    //para Usuario
+}
+
+function checkCorreo (valor){
+    //para Email
+}
+
+function checkContrasena (valor){
+    //para Contraseña
+}
+
+function checkConfirmContrasena (valor1, valor2){
+    //para Confirmar Contraseña
+}
