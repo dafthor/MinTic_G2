222d839b4ba0a5119ef4579ec17e0b07e1a8167a
Pipe <dafthor@gmail.com>
2021-09-27 20:52:14 -0500

    Import y Export

--- form.js
+++ form.js
@@ -1,104 +1,78 @@
-const formulario =  document.getElementById('form-registro');
-const inputs = document.querySelectorAll('#form-registro input');
+//! Sprint 2
 
-formulario.addEventListener('submit',(e) => {  //Funcion flecha que se ejecuta al presionar REGISTRAR 
-    e.preventDefault(); //Funcion para evitar envio vacio del formulario
+//? *Campo username*
+function checkUsername(valor) {
+  if (
+    valor == null ||
+    valor.length > 30 ||
+    valor.length == 0 ||
+    /^\s+$/.test(valor)
+  ) {
+    // Validar campo de texto obligatorio
+    return false;
+  } else if (/^[a-z0-9A-Z]{1,30}$/.test(valor)) {
+    // Validar condicion numeros y letras
+    return true;
+  } else {
+    return false;
+  }
+}
 
-        // Acceso  a la informacion ingresada en el formulario
-        const inputUsername = document.getElementById('username').value;
-        const inputCorreo = document.getElementById('correo').value;
-        const inputPassword = document.getElementById('contrasena').value;
-        const inputConfirmarContrasena = document.getElementById('confirmContrasena').value;
+//? *Campo e-mail*
+function checkCorreo(valor) {
+  if (valor == null || valor.length == 0 || /^\s+$/.test(valor)) {
+    // Validar campo de texto obligatorio
+    return false;
+  } else if (!/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(valor)) {
+    //Validar condicion correo electronico
+    return false;
+  } else {
+    return true;
+  }
+}
 
-        //! Sprint 2
+//? *Campo contrasena*
+function checkContrasena(valor) {
+  if (valor == null || valor.length == 0 || /^\s+$/.test(valor)) {
+    // Validar campo de texto obligatorio
+    return false;
+  } else if (/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,20}$/.test(valor)) {
+    // Validar condiciones contrasena
+    return true;
+  } else {
+    return false;
+  }
+}
 
-        //? *Campo username*
-        function checkUsername(valor1){
-            if (valor1 == null || valor1.length > 30 || valor1.length == 0 || /^\s+$/.test(valor1) ) { // Validar campo de texto obligatorio
-                return false;
-            }
-            else if ( /^[a-z0-9A-Z]{1,30}$/.test(valor1) ) { // Validar condicion numeros y letras
-                return true;
-            }else{
-                return false;
-                }
-            
-        }
+//? *Campo confirma contrasena*
+function checkConfirmContrasena(valor1, valor2) {
+  if (valor2 == null || valor2.length == 0 || /^\s+$/.test(valor2)) {
+    // Validar campo de texto obligatorio
+    return false;
+  } else if (
+    /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,20}$/.test(valor2) &&
+    valor2 == valor1
+  ) {
+    // Validar condiciones contrasena y su igualdad con la contrasena del cuadro anterior
+    return true;
+  } else {
+    return false;
+  }
+}
 
-        //Variable usada en la validacion final
-        console.log("Username: " + valor1);
-        console.log(checkUsername(valor1));
-
-        //? *Campo e-mail*
-        
-        function checkCorreo(valor2){
-            if (valor2 == null || valor2.length == 0 || /^\s+$/.test(valor2) ) { // Validar campo de texto obligatorio
-                return false;
-            }
-            else if( !(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(valor2)) ) { //Validar condicion correo electronico
-                return false;
-            }else{
-                return true;
-                }
-            }
-
-         //Variable usada en la validacion final
-        console.log("Correo: "+ valor2);
-        console.log(checkCorreo(valor2));
-
-        //? *Campo contrasena*
-        function checkContrasena(valor3){
-            if (valor3 == null || valor3.length == 0 || /^\s+$/.test(valor3) ) { // Validar campo de texto obligatorio
-                return false;
-            }
-            else if (/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,20}$/.test(valor3) ) { // Validar condiciones contrasena 
-                return true;
-            }else{
-                return false;
-                }
-            }
-  //Variable usada en la validacion final
-        console.log("Contrasena:"+ valor3);
-        console.log(checkContrasena(valor3));
-
-        //? *Campo confirma contrasena*
-        
-        function checkConfirmContrasena(valor3,valor4){
-            if (valor4 == null || valor4.length == 0 || /^\s+$/.test(valor4) ) { // Validar campo de texto obligatorio
-                return false;
-            }
-            else if (/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,20}$/.test(valor4)  && valor4==valor3) { // Validar condiciones contrasena y su igualdad con la contrasena del cuadro anterior
-                return true;
-            }else{
-                return false;
-                }
-            } 
-
-         //Variable usada en la validacion final
-        console.log("Confirmacion contrasena: " + valor4);
-        console.log(checkConfirmContrasena(valor3,valor4));
-
-    if (a==true && a==b && a==c && a==d){  // Verificacion final de todos los campos
-        formulario.reset();
-        console.log("Resultado comprobacion: Campos correctos");
-        alert("Formulario enviado correctamente");
-    }
-    else{
-        console.log("Resultado comprobacion: Campos incorrectos");
-        alert("Error en uno o mas campos");
-    }
-
-});
-
+/*
 module.exports = checkUsername;
 module.exports = checkCorreo;
 module.exports = checkContrasena;
 module.exports = checkConfirmContrasena;
+*/
+/*
+module.exports = {
+  checkUsername,
+  checkCorreo,
+  checkContrasena,
+  checkConfirmContrasena,
+};
+*/
 
-//module.exports = {
-   // checkUsername,
-   // checkCorreo,
-    //checkContrasena,
-  //  checkConfirmContrasena
-//}
-
+export { checkUsername, checkCorreo, checkContrasena, checkConfirmContrasena };
--- verificacion.js
+++ verificacion.js
@@ -1,40 +1,53 @@
-import * as form from './form';
+console.log("archivo verificacion...");
+import {
+  checkConfirmContrasena,
+  checkContrasena,
+  checkCorreo,
+  checkUsername,
+} from "./form.js";
+
 // import * as registro from './registro';
 
-const formulario =  document.getElementById('form-registro');
-const inputs = document.querySelectorAll('#form-registro input');
+const formulario = document.getElementById("form-registro");
+// const inputs = document.querySelectorAll("#form-registro input");
 
-formulario.addEventListener('submit',(e) => {  //Funcion flecha que se ejecuta al presionar REGISTRAR 
-    e.preventDefault(); //Funcion para evitar envio vacio del formulario
+formulario.addEventListener("submit", (e) => {
+  //Funcion flecha que se ejecuta al presionar REGISTRAR
+  e.preventDefault(); //Funcion para evitar envio vacio del formulario
 
-        // Acceso  a la informacion ingresada en el formulario
-        var inputUsername = document.getElementById('username');
-        const inputCorreo = document.getElementById('correo');
-        const inputPassword = document.getElementById('contrasena');
-        const inputConfirmarContrasena = document.getElementById('confirmContrasena');
+  // Acceso  a la informacion ingresada en el formulario
+  var inputUsername = document.getElementById("username");
+  const inputCorreo = document.getElementById("correo");
+  const inputPassword = document.getElementById("contrasena");
+  const inputConfirmarContrasena = document.getElementById("confirmContrasena");
 
-        //! Sprint 2
+  //! Sprint 2
 
-        //? *Campo username*
-        //Variable usada en la validacion final
-        console.log("Validacion Usuario.. "+ form.checkUsername(inputUsername.value));
-
-        //? *Campo e-mail*
-         //Variable usada en la validacion final
-        console.log("Correo: "+ form.checkCorreo(inputCorreo.value));
-        
-
-        //? *Campo contrasena*
-        
+  //? *Campo username*
   //Variable usada en la validacion final
-        console.log("Contrasena:"+ form.checkContrasena(inputPassword.value));
+  console.log("Validacion Usuario.. " + checkUsername(inputUsername.value));
 
-        //? *Campo confirma contrasena*
+  //? *Campo e-mail*
+  //Variable usada en la validacion final
+  console.log("Correo: " + checkCorreo(inputCorreo.value));
 
-         //Variable usada en la validacion final
-        console.log("Confirmacion contrasena: " + form.checkConfirmContrasena(inputPassword.value, inputConfirmarContrasena.value));
+  //? *Campo contrasena*
 
-    /* if (a==true && a==b && a==c && a==d){  // Verificacion final de todos los campos
+  //Variable usada en la validacion final
+  console.log("Contrasena:" + checkContrasena(inputPassword.value));
+
+  //? *Campo confirma contrasena*
+
+  //Variable usada en la validacion final
+  console.log(
+    "Confirmacion contrasena: " +
+      checkConfirmContrasena(
+        inputPassword.value,
+        inputConfirmarContrasena.value
+      )
+  );
+
+  /* if (a==true && a==b && a==c && a==d){  // Verificacion final de todos los campos
         formulario.reset();
         console.log("Resultado comprobacion: Campos correctos");
         alert("Formulario enviado correctamente");
@@ -43,5 +56,4 @@ formulario.addEventListener('submit',(e) => {  //Funcion flecha que se ejecuta a
         console.log("Resultado comprobacion: Campos incorrectos");
         alert("Error en uno o mas campos");
     } */
-
 });
