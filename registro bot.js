d7d60e706924e549a3e704f4e95e446c3c7769c8
Pipe <dafthor@gmail.com>
2021-10-01 22:22:14 -0500

    Funcion username con forEach

--- registro bot.js
+++ registro bot.js
@@ -4,66 +4,33 @@ function agregarRegistro(inputUsername, inputCorreo, inputPassword) {
   arregloNuevo.push(inputCorreo);
   arregloNuevo.push(inputPassword);
   // arregloNuevo.push(inputConfirmarContrasena);
-
   arreglo.push(arregloNuevo); //
   return 0;
 }
-function filtrarUsername(entrada) {
-  if (/^[a-zA-Z_/.+-]{1,30}$/.test(entrada)) {
-    // Validar condicion numeros
-    return false;
-  } else {
-    return true;
-  }
-}
+
 let arregloReg = [];
 function obtenerUsername(arreglo) {
   //Funcion pedida
-
-  let arregloT = [];
-  for (let i = 0; i < arreglo.length; i++) {
-    let arregloT = [];
-    //for (let j = 0; j < 1; j++)
-    //var arregloT = arreglo[i][0].filter(filtrarUsername)   //PEndiente de mejorar
-    if (filtrarUsername(arreglo[i][0]) == false) {
-      continue;
-    } else {
-      for (let k = 0; k < 4; k++) {
-        arregloT.push(arreglo[i][k]);
-      }
+  arregloReg = [];
+  arreglo.forEach(function (element) {
+    if (/^[a-zA-Z_/.+-]{1,30}$/.test(element[0])) {
+      arregloReg.push(element);
     }
-    arregloReg.push(arregloT);
-  }
+  });
   return arregloReg;
 }
-function recoCorreo(correo) {
-  // El correo tiene o no hotmail.com?
-  if (/^([a-z0-9_\.-]+)@(hotmail).(com)$/.test(correo)) {
-    // Validar condicion numeros
-    return true;
-  } else {
-    return false;
-  }
-}
-let matrizCorreos = [];
 
+let matrizCorreos = [];
 function filtrarCorreo(arreglo) {
-  let arregloT2 = [];
-  for (let i = 0; i < arreglo.length; i++) {
-    let arregloT2 = [];
-    for (let j = 1; j < 2; j++) {
-      if (recoCorreo(arreglo[i][j]) == false) {
-        continue;
-      } else {
-        for (let k = 0; k < 4; k++) {
-          arregloT2.push(arreglo[i][k]);
-        }
-      }
-      matrizCorreos.push(arregloT2);
+  matrizCorreos = [];
+  arreglo.forEach(function (element) {
+    if (/^([a-z0-9_\.-]+)@(hotmail).(com)$/.test(element[1])) {
+      matrizCorreos.push(element);
     }
-  }
+  });
   return matrizCorreos;
 }
+
 //?Export
 module.export = {
   filtrarCorreo,
--- registro.js
+++ registro.js
@@ -5,7 +5,6 @@ let arreglo = [];
 
 //Agregar registro
 var arregloNuevo = [];
-
 function agregarRegistro(inputUsername, inputCorreo, inputPassword) {
   arregloNuevo = [];
   arregloNuevo.push(inputUsername);
@@ -21,53 +20,66 @@ function agregarRegistro(inputUsername, inputCorreo, inputPassword) {
 } //Cierre funcion agregarRegistro()
 //agregarRegistro();
 
-console.log("Arreglo actual 'Registro.js': ");
-console.log(arreglo); //Arreglo global despues de anadir registros
+// console.log("Arreglo actual 'Registro.js': ");
+// console.log(arreglo); //Arreglo global despues de anadir registros
 
 // Obtener Username
-function filtrarUsername(entrada) {
-  if (/^[a-zA-Z_/.+-]{1,30}$/.test(entrada)) {
-    // Validar condicion numeros
-    return false;
-  } else {
-    return true;
-  }
-}
+// function filtrarUsername(entrada) {
+//   if (/^[a-zA-Z_/.+-]{1,30}$/.test(entrada)) {
+//     // Validar condicion numeros
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
 
-export let arregloReg = [];
+let arregloReg = [];
 function obtenerUsername(arreglo) {
   //Funcion pedida
   arregloReg = [];
-  let arregloT = [];
-  for (let i = 0; i < arreglo.length; i++) {
-    let arregloT = [];
-    //for (let j = 0; j < 1; j++)
-    //var arregloT = arreglo[i][0].filter(filtrarUsername)   //PEndiente de mejorar
-    if (filtrarUsername(arreglo[i][0]) == false) {
-      continue;
+  arreglo.forEach(function (element) {
+    if (/^[a-zA-Z_/.+-]{1,30}$/.test(element[0])) {
+      //continue
     } else {
-      for (let k = 0; k < 3; k++) {
-        arregloT.push(arreglo[i][k]);
-      }
+      arregloReg.push(element);
     }
-    arregloReg.push(arregloT);
-  }
+  });
   return arregloReg;
 }
+// export let arregloReg = [];
+// function obtenerUsername(arreglo) {
+//   //Funcion pedida
+//   arregloReg = [];
+//   let arregloT = [];
+//   for (let i = 0; i < arreglo.length; i++) {
+//     let arregloT = [];
+//     //for (let j = 0; j < 1; j++)
+//     //var arregloT = arreglo[i][0].filter(filtrarUsername)   //PEndiente de mejorar
+//     if (filtrarUsername(arreglo[i][0]) == false) {
+//       continue;
+//     } else {
+//       for (let k = 0; k < 3; k++) {
+//         arregloT.push(arreglo[i][k]);
+//       }
+//     }
+//     arregloReg.push(arregloT);
+//   }
+//   return arregloReg;
+// }
 
-console.log("El arreglo filtrado por username es 'registro.js': ");
-console.log(obtenerUsername(arreglo));
+// console.log("El arreglo filtrado por username es 'registro.js': ");
+// console.log(obtenerUsername(arreglo));
 
 //Filtrar Correo
-function recoCorreo(correo) {
-  // El correo tiene o no hotmail.com?
-  if (/^([a-z0-9_\.-]+)@(hotmail).(com)$/.test(correo)) {
-    // Validar condicion numeros
-    return true;
-  } else {
-    return false;
-  }
-}
+// function recoCorreo(correo) {
+//   // El correo tiene o no hotmail.com?
+//   if (/^([a-z0-9_\.-]+)@(hotmail).(com)$/.test(correo)) {
+//     // Validar condicion numeros
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
 // let matrizCorreos = [];
 
@@ -104,7 +116,7 @@ function filtrarCorreo(arreglo) {
 //?Export
 export { filtrarCorreo, obtenerUsername, agregarRegistro, arreglo };
 
-console.log("El arreglo filtrado por correo es 'registro.js': ");
-console.log(filtrarCorreo(arreglo));
+// console.log("El arreglo filtrado por correo es 'registro.js': ");
+// console.log(filtrarCorreo(arreglo));
 
 // module.exports = { filtrarCorreo, obtenerUsername, agregarRegistro, arreglo };
