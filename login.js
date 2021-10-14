60dce7d680f5c756ef237b0a6294db12e8235f5d
Mateo Rojas <marojas4126@gmail.com>
2021-10-14 11:00:30 -0500

    Parcial login.js y cambio id login.html

--- login.js
+++ login.js
@@ -1,21 +1,50 @@
-const formulario = document.getElementById("formulario");
+const formulario = document.getElementById("form-login");
 const capital = "budapest";
-let registros = []
-
-function Login (){
-
+let registros = [];
+var arregloNuevo = [];
+function login() {
+  var userCo = document.getElementById('username/correo').value;
+  var contrasena = document.getElementById('contrasena').value;
+  var captcha = validarCAPTCHA();
+  // let lista = [];
+  registros.forEach(function (element){
+    if (userCo == element[0] || userCo == element[1]){
+      if(contrasena == element[2]){
+        if(captcha){
+          console.log(element)
+          alert("Sesion iniciada correctamente")
+          break
+        }else{
+          alert("Captcha incorrecto")
+        }
+      }else{
+        alert("Contraseña incorrecta")
+      }
+    }else{
+      
+    }
+  })
+  registros.forEach(function (element){
+    if (userCo == element[0] || userCo == element[1]){
+      
+    }
+  })
 }
 
-function agregraRegistros(){
-    
+function agregarRegistros() {
+  arregloNuevo.push(document.getElementById("username").value);
+  arregloNuevo.push(document.getElementById("correo").value);
+  arregloNuevo.push(document.getElementById("contrasena").value);
+
+  registros.push(arregloNuevo); //
+
+  //console.log("guarde correctamente 'agregarRegistro' arreglo:");
+  console.log(registros);
 }
-
-
-
 
 formulario.addEventListener("submit", (e) => {
   e.preventDefault(); //Funcion para evitar envio vacio del formulario
-
+  login();
   //? llamado a Validacion captcha
   if (validarCAPTCHA()) {
     alert("RESPUESTAS IGUALES");
